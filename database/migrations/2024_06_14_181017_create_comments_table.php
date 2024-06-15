4775bbec13c5e6cf3a21ed55477f7f191d1c9086
tmeunier <theo.meunier41@gmail.com>
2024-06-15 12:44:18 +0200

    initial commit

---
+++ database/migrations/2024_06_14_181017_create_comments_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\Article;
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->boolean('is_reported')->default(false);
+
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignIdFor(Article::class)
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignIdFor(Comment::class)
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
