0344c3df79f4cc996de436ec5ebcd6a3c708a48d
tmeunier <theo.meunier41@gmail.com>
2024-07-14 21:29:49 +0200

    fix: erreur title and redirection after register

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
+    <x-slot name="title">Blog</x-slot>
+    <x-slot name="description">On vous présente l'article {{ $article->title }}</x-slot>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ $article->title }}
