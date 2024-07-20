8b6bb1e29804ca8fa2751b57c8054b114ed0d03c
Tinou95 <quentin95170@hotmail.fr>
2024-07-20 22:00:41 +0200

    style: form

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -1,4 +1,3 @@
-<!-- resources/views/contact/show.blade.php -->
 <x-app-layout>
     <x-slot name="title">Contactez-nous</x-slot>
     <x-slot name="description">Page de contact pour nous envoyer un message.</x-slot>
@@ -8,12 +7,12 @@
         </h2>
     </x-slot>
 
-    <div class="py-12 bg-gray-50">
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                <div class="px-6 py-5 bg-gray-100 border-b border-gray-200">
+                <div class="px-6 py-5 bg-white border-b border-gray-400">
                     <h3 class="text-lg font-semibold text-gray-900">Formulaire de contact</h3>
-                </div>
+                </div>                
                 <div class="border-t border-gray-200">
                     <div class="px-6 py-5">
                         @if (session('success'))
